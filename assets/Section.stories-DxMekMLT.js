d63d6137f44167508e99804a454f3dd84d82f0cd
yomo-yohaku <yomo.yohaku@gmail.com>
2025-09-18 22:49:29 +0900

    Updates

---
+++ assets/Section.stories-DxMekMLT.js
@@ -0,0 +1,380 @@
+/* empty css                */import{c}from"./index-DPIsAUKd.js";const e=`<div class="tw:bg-slate-200 tw:py-[40px]">
+  <p
+    class="tw:font-bold tw:text-[16px] tw:md:text-[24px] tw:mb-[16px] tw:md:mb-[24px] tw:text-center"
+  >
+    右寄せセクション
+  </p>
+
+  <div class="p-section-right-over">
+    <div class="p-section-right-over__container">
+      <ul class="p-cards-event01">
+        <li class="c-card-event01">
+          <a href="#" class="c-card-event01__link">
+            <figure class="c-card-event01__amount">
+              <img
+                src="./assets/images/card/image_01.jpg"
+                alt="タイトルタイトルタイトルタイトル"
+                loading="lazy"
+                decoding="async"
+                width="660"
+                height="410"
+                class="c-card-event01__image"
+              />
+            </figure>
+            <div class="c-card-event01__wrap">
+              <p class="c-card-event01__title">
+                タイトルタイトルタイトルタイトル
+              </p>
+              <p class="c-card-event01__text">
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              </p>
+              <div class="c-card-event01__schedule">
+                08/09(土), 08/10(日), 08/11(月), 08/15(金), 08/16(土)
+                <span> 他</span>
+              </div>
+            </div>
+          </a>
+          <div class="c-card-event01__buttons">
+            <div class="c-card-event01__button --detail">
+              <a href="#" class="c-card-event01__button-link"> 詳細を見る </a>
+            </div>
+            <div class="c-card-event01__button --reservation">
+              <a href="#" target="_blank" class="c-card-event01__button-link">
+                予約する
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="c-card-event01">
+          <a href="#" class="c-card-event01__link">
+            <figure class="c-card-event01__amount">
+              <img
+                src="./assets/images/card/image_01.jpg"
+                alt="タイトルタイトルタイトルタイトル"
+                loading="lazy"
+                decoding="async"
+                width="660"
+                height="410"
+                class="c-card-event01__image"
+              />
+            </figure>
+            <div class="c-card-event01__wrap">
+              <p class="c-card-event01__title">
+                タイトルタイトルタイトルタイトル
+              </p>
+              <p class="c-card-event01__text">
+                テキストテキストテキストテキスト
+              </p>
+              <div class="c-card-event01__schedule">
+                08/09(土), 08/10(日), 08/11(月), 08/15(金), 08/16(土)
+                <span> 他</span>
+              </div>
+            </div>
+          </a>
+          <div class="c-card-event01__buttons">
+            <div class="c-card-event01__button --detail">
+              <a href="#" class="c-card-event01__button-link"> 詳細を見る </a>
+            </div>
+            <div class="c-card-event01__button --reservation">
+              <a href="#" target="_blank" class="c-card-event01__button-link">
+                予約する
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="c-card-event01">
+          <a href="#" class="c-card-event01__link">
+            <figure class="c-card-event01__amount">
+              <img
+                src="./assets/images/card/image_01.jpg"
+                alt="タイトルタイトルタイトルタイトル"
+                loading="lazy"
+                decoding="async"
+                width="660"
+                height="410"
+                class="c-card-event01__image"
+              />
+            </figure>
+            <div class="c-card-event01__wrap">
+              <p class="c-card-event01__title">
+                タイトルタイトルタイトルタイトル
+              </p>
+              <p class="c-card-event01__text">
+                テキストテキストテキストテキストテキストテキスト
+              </p>
+              <div class="c-card-event01__schedule">
+                08/09(土), 08/10(日), 08/11(月), 08/15(金), 08/16(土)
+                <span> 他</span>
+              </div>
+            </div>
+          </a>
+          <div class="c-card-event01__buttons">
+            <div class="c-card-event01__button --detail">
+              <a href="#" class="c-card-event01__button-link"> 詳細を見る </a>
+            </div>
+            <div class="c-card-event01__button --reservation">
+              <a href="#" target="_blank" class="c-card-event01__button-link">
+                予約する
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="c-card-event01">
+          <a href="#" class="c-card-event01__link">
+            <figure class="c-card-event01__amount">
+              <img
+                src="./assets/images/card/image_01.jpg"
+                alt="タイトルタイトルタイトルタイトル"
+                loading="lazy"
+                decoding="async"
+                width="660"
+                height="410"
+                class="c-card-event01__image"
+              />
+            </figure>
+            <div class="c-card-event01__wrap">
+              <p class="c-card-event01__title">
+                タイトルタイトルタイトルタイトル
+              </p>
+              <p class="c-card-event01__text">
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              </p>
+              <div class="c-card-event01__schedule">
+                08/09(土), 08/10(日), 08/11(月), 08/15(金), 08/16(土)
+                <span> 他</span>
+              </div>
+            </div>
+          </a>
+          <div class="c-card-event01__buttons">
+            <div class="c-card-event01__button --detail">
+              <a href="#" class="c-card-event01__button-link"> 詳細を見る </a>
+            </div>
+            <div class="c-card-event01__button --reservation">
+              <a href="#" target="_blank" class="c-card-event01__button-link">
+                予約する
+              </a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="tw:py-[40px]">
+    <p
+      class="tw:font-bold tw:text-[16px] tw:md:text-[24px] tw:mb-[16px] tw:md:mb-[24px] tw:text-center"
+    >
+      通常セクション
+    </p>
+
+    <div class="tw:w-[90%] tw:md:w-[1000px] tw:mx-auto">
+      <ul class="p-cards-event01">
+        <li class="c-card-event01">
+          <a href="#" class="c-card-event01__link">
+            <figure class="c-card-event01__amount">
+              <img
+                src="./assets/images/card/image_01.jpg"
+                alt="タイトルタイトルタイトルタイトル"
+                loading="lazy"
+                decoding="async"
+                width="660"
+                height="410"
+                class="c-card-event01__image"
+              />
+            </figure>
+            <div class="c-card-event01__wrap">
+              <p class="c-card-event01__title">
+                タイトルタイトルタイトルタイトル
+              </p>
+              <p class="c-card-event01__text">
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              </p>
+              <div class="c-card-event01__schedule">
+                08/09(土), 08/10(日), 08/11(月), 08/15(金), 08/16(土)
+                <span> 他</span>
+              </div>
+            </div>
+          </a>
+          <div class="c-card-event01__buttons">
+            <div class="c-card-event01__button --detail">
+              <a href="#" class="c-card-event01__button-link"> 詳細を見る </a>
+            </div>
+            <div class="c-card-event01__button --reservation">
+              <a href="#" target="_blank" class="c-card-event01__button-link">
+                予約する
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="c-card-event01">
+          <a href="#" class="c-card-event01__link">
+            <figure class="c-card-event01__amount">
+              <img
+                src="./assets/images/card/image_01.jpg"
+                alt="タイトルタイトルタイトルタイトル"
+                loading="lazy"
+                decoding="async"
+                width="660"
+                height="410"
+                class="c-card-event01__image"
+              />
+            </figure>
+            <div class="c-card-event01__wrap">
+              <p class="c-card-event01__title">
+                タイトルタイトルタイトルタイトル
+              </p>
+              <p class="c-card-event01__text">
+                テキストテキストテキストテキスト
+              </p>
+              <div class="c-card-event01__schedule">
+                08/09(土), 08/10(日), 08/11(月), 08/15(金), 08/16(土)
+                <span> 他</span>
+              </div>
+            </div>
+          </a>
+          <div class="c-card-event01__buttons">
+            <div class="c-card-event01__button --detail">
+              <a href="#" class="c-card-event01__button-link"> 詳細を見る </a>
+            </div>
+            <div class="c-card-event01__button --reservation">
+              <a href="#" target="_blank" class="c-card-event01__button-link">
+                予約する
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="c-card-event01">
+          <a href="#" class="c-card-event01__link">
+            <figure class="c-card-event01__amount">
+              <img
+                src="./assets/images/card/image_01.jpg"
+                alt="タイトルタイトルタイトルタイトル"
+                loading="lazy"
+                decoding="async"
+                width="660"
+                height="410"
+                class="c-card-event01__image"
+              />
+            </figure>
+            <div class="c-card-event01__wrap">
+              <p class="c-card-event01__title">
+                タイトルタイトルタイトルタイトル
+              </p>
+              <p class="c-card-event01__text">
+                テキストテキストテキストテキストテキストテキスト
+              </p>
+              <div class="c-card-event01__schedule">
+                08/09(土), 08/10(日), 08/11(月), 08/15(金), 08/16(土)
+                <span> 他</span>
+              </div>
+            </div>
+          </a>
+          <div class="c-card-event01__buttons">
+            <div class="c-card-event01__button --detail">
+              <a href="#" class="c-card-event01__button-link"> 詳細を見る </a>
+            </div>
+            <div class="c-card-event01__button --reservation">
+              <a href="#" target="_blank" class="c-card-event01__button-link">
+                予約する
+              </a>
+            </div>
+          </div>
+        </li>
+        <li class="c-card-event01">
+          <a href="#" class="c-card-event01__link">
+            <figure class="c-card-event01__amount">
+              <img
+                src="./assets/images/card/image_01.jpg"
+                alt="タイトルタイトルタイトルタイトル"
+                loading="lazy"
+                decoding="async"
+                width="660"
+                height="410"
+                class="c-card-event01__image"
+              />
+            </figure>
+            <div class="c-card-event01__wrap">
+              <p class="c-card-event01__title">
+                タイトルタイトルタイトルタイトル
+              </p>
+              <p class="c-card-event01__text">
+                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              </p>
+              <div class="c-card-event01__schedule">
+                08/09(土), 08/10(日), 08/11(月), 08/15(金), 08/16(土)
+                <span> 他</span>
+              </div>
+            </div>
+          </a>
+          <div class="c-card-event01__buttons">
+            <div class="c-card-event01__button --detail">
+              <a href="#" class="c-card-event01__button-link"> 詳細を見る </a>
+            </div>
+            <div class="c-card-event01__button --reservation">
+              <a href="#" target="_blank" class="c-card-event01__button-link">
+                予約する
+              </a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+`,a=`.p-section-right-over {
+  overflow: hidden;
+}
+
+.p-section-right-over__container {
+  position: relative;
+  z-index: 1;
+  width: 90%;
+  padding: 24px;
+  margin-inline: auto;
+}
+@media screen and (min-width: 768px) {
+  .p-section-right-over__container {
+    width: 1000px;
+    padding: 40px;
+  }
+}
+
+.p-section-right-over__container::before {
+  position: absolute;
+  inset: 0;
+  z-index: -1;
+  width: 100vw;
+  height: 100%;
+  content: "";
+  background-color: #f8fafc;
+  border-radius: 16px 0 0 16px;
+}
+@media screen and (min-width: 768px) {
+  .p-section-right-over__container::before {
+    border-radius: 32px 0 0 32px;
+  }
+}
+`,d={title:"Project/Section"},n={render:()=>`
+    <style>
+        ${c}
+        ${a}
+      </style>
+    ${e}
+  `,parameters:{sourceCode:[{name:"HTML",code:e},{name:"CSS",code:a}]}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  render: () => \`
+    <style>
+        \${cssCardEvent}
+        \${cssRightOver}
+      </style>
+    \${rightOver}
+  \`,
+  parameters: {
+    sourceCode: [{
+      name: "HTML",
+      code: rightOver
+    }, {
+      name: "CSS",
+      code: cssRightOver
+    }]
+  }
+}`,...n.parameters?.docs?.source}}};const i=["RightOver"];export{n as RightOver,i as __namedExportsOrder,d as default};
