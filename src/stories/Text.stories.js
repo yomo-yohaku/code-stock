ad367c7f813997b85342b22a61e08b8fc695e499
yomo-yohaku <yomo.yohaku@gmail.com>
2025-09-23 17:10:14 +0900

    「COMPONENT ＞ Text ＞ Flowing」を追加

---
+++ src/stories/Text.stories.js
@@ -0,0 +1,21 @@
+import "destyle.css";
+
+import flowing from "../code/component/text/flowing/index.html?raw";
+import cssFlowing from "../code/component/text/flowing/index.css?raw";
+
+export default {
+  title: "Component/Text",
+};
+
+export const Flowing = {
+  render: () => `
+    <style>${cssFlowing}</style>
+    ${flowing}
+  `,
+  parameters: {
+    sourceCode: [
+      { name: "HTML", code: flowing },
+      { name: "CSS", code: cssFlowing },
+    ],
+  },
+};
