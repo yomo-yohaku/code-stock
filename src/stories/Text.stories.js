f301cfd4b715cbe4d39c1d1b14ada7e1d405c2d1
yomo-yohaku <yomo.yohaku@gmail.com>
2025-09-24 22:06:17 +0900

    「COMPONENT ＞ Text ＞ Link」を追加

--- src/stories/Text.stories.js
+++ src/stories/Text.stories.js
@@ -3,6 +3,9 @@ import "destyle.css";
 import flowing from "../code/component/text/flowing/index.html?raw";
 import cssFlowing from "../code/component/text/flowing/index.css?raw";
 
+import link from "../code/component/text/link/index.html?raw";
+import cssLink from "../code/component/text/link/index.css?raw";
+
 export default {
   title: "Component/Text",
 };
@@ -16,6 +19,19 @@ export const Flowing = {
     sourceCode: [
       { name: "HTML", code: flowing },
       { name: "CSS", code: cssFlowing },
+    ],
+  },
+};
+
+export const Link = {
+  render: () => `
+    <style>${cssLink}</style>
+    ${link}
+  `,
+  parameters: {
+    sourceCode: [
+      { name: "HTML", code: link },
+      { name: "CSS", code: cssLink },
     ],
   },
 };
