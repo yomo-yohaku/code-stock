fcc266cbb9a64db83b4c127f1f0a0ebb99f7332a
yomo-yohaku <yomo.yohaku@gmail.com>
2025-10-11 10:36:25 +0900

    「PROJECT ＞ Table ＞ Profile」を追加

---
+++ src/stories/Table.stories.js
@@ -0,0 +1,21 @@
+import "destyle.css";
+
+import profile from "../code/project/table/profile/index.html?raw";
+import cssProfile from "../code/project/table/profile/index.css?raw";
+
+export default {
+  title: "Project/Table",
+};
+
+export const Profile = {
+  render: () => `
+    <style>${cssProfile}</style>
+    ${profile}
+  `,
+  parameters: {
+    sourceCode: [
+      { name: "HTML", code: profile },
+      { name: "CSS", code: cssProfile },
+    ],
+  },
+};
