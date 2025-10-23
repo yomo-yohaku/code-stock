c4b702d4c9e1f7d30627cf84164ffdf830a89c24
yomo-yohaku <yomo.yohaku@gmail.com>
2025-10-23 21:12:28 +0900

    「COMPONENT ＞ Heading ＞ Title」を追加

---
+++ src/stories/Heading.stories.js
@@ -0,0 +1,21 @@
+import "destyle.css";
+
+import title from "../code/component/heading/title/index.html?raw";
+import cssTitle from "../code/component/heading/title/index.css?raw";
+
+export default {
+  title: "Component/Heading",
+};
+
+export const Title = {
+  render: () => `
+    <style>${cssTitle}</style>
+    ${title}
+  `,
+  parameters: {
+    sourceCode: [
+      { name: "HTML", code: title },
+      { name: "CSS", code: cssTitle },
+    ],
+  },
+};
